116d0c26fc5f0a84614879a7325f051a51360a1a
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-02-01 21:00:47 +0100

    add multiple checkbox, YY_XYZ, possibility to add values

--- init.rb
+++ init.rb
@@ -7,25 +7,8 @@ Redmine::Plugin.register :redmine_cf_autoadd do
   author_url 'https://github.com/Atrasoftware'
 
   settings :default => {
-        'auto_increment_globally'=> true
+        'auto_increment_globally'=> true,
+        'auto_add_uniquness' => false
    }, :partial => 'settings/auto_increment'
-  class AutoAddHooks < Redmine::Hook::ViewListener
-    def controller_issues_new_before_save(context = {})
-      issue = context[:issue]
-      settings = Setting.send "plugin_redmine_cf_autoadd"
-      issue_cfs = CustomField.where("type= 'IssueCustomField' and field_format in ('int', 'string')").detect{|cf| settings[cf.name] == "true" }
-      unless issue_cfs.nil?
-        if settings['auto_increment_globally'] == "true"
-            max = issue_cfs.custom_values.map(&:value).max.to_i.next.to_s
-        else
-          project = issue.project
-          issues_id = project.issues.map(&:id)
-          max = CustomValue.where("customized_type= 'issue' and customized_id in(?) and custom_field_id = ?",
-                                  issues_id, issue_cfs.id).map(&:value).max.to_i.next.to_s
-        end
-        detect_cf = issue.visible_custom_field_values.detect{|cf| cf.custom_field == issue_cfs}
-        detect_cf.value = max
-      end
-    end
-  end
 end
+require 'redmine_auto_add_hook'
