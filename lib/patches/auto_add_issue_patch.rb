18eaf3f83b6015c120e6206ac4f251700d3080d7
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-02-01 21:46:32 +0100

    uniqueness cf

--- lib/patches/auto_add_issue_patch.rb
+++ lib/patches/auto_add_issue_patch.rb
@@ -11,9 +11,9 @@ module  Patches
 
         def uniqueness_cf
           settings = Setting.send "plugin_redmine_cf_autoadd"
-          issues_cfs = CustomField.where("type= 'IssueCustomField' and field_format in ('string')").select{|cf| settings[cf.name] == "true" }
           uniqueness = settings['auto_add_uniquness'] || false
           if uniqueness == 'true'
+            issues_cfs = CustomField.where("type= 'IssueCustomField' and field_format in ('string')").select{|cf| settings[cf.name] == "true" }
             issues_cfs.each do |issue_cfs|
               detect_cf = visible_custom_field_values.detect{|cf| cf.custom_field == issue_cfs}
               check_uniq = CustomValue.where("customized_type= 'issue' and custom_field_id = ? and value = ?",
