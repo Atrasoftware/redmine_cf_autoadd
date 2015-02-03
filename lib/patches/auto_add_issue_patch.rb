33a7c2eb834cc62e6d0f662a7a4e5e4221262690
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-02-03 16:56:00 +0100

    uniqueness in updating issues

--- lib/patches/auto_add_issue_patch.rb
+++ lib/patches/auto_add_issue_patch.rb
@@ -8,6 +8,7 @@ module  Patches
       base.send(:include, InstanceMethods)
       base.class_eval do
         before_create :uniqueness_cf
+        before_update :uniqueness_cf_update
 
         def uniqueness_cf
           settings = Setting.send "plugin_redmine_cf_autoadd"
@@ -35,6 +36,32 @@ module  Patches
 
         end
 
+        def uniqueness_cf_update
+          settings = Setting.send "plugin_redmine_cf_autoadd"
+          issues_cfs = CustomField.where("type= 'IssueCustomField' and field_format in ('string')").select{|cf| settings[cf.name] == "true" }
+          issues_cfs.each do |issue_cfs|
+            uniqueness = settings["cf_uniq_#{issue_cfs.name}"] || false
+            detect_cf = visible_custom_field_values.detect{|cf| cf.custom_field == issue_cfs}
+            if detect_cf
+              if uniqueness == 'true'
+                check_uniq = CustomValue.where("customized_type= 'issue' and custom_field_id = ? and value = ? and and customized_id <> ?",
+                                               issue_cfs.id, detect_cf.value, id)
+              else
+                # check uniqueness for that project
+                project_issues = project.issues.map(&:id) || []
+                project_issues.delete(id)
+                check_uniq = CustomValue.where("customized_type= 'issue' and customized_id in (?) and custom_field_id = ? and value = ?  ",
+                                               project_issues, issue_cfs.id, detect_cf.value)
+              end
+
+              unless check_uniq.empty?
+                errors[:base]<< "#{issue_cfs.name} #{l(:value_must_be_uniq)}"
+                return false
+              end
+            end
+          end
+
+        end
       end
     end
   end
