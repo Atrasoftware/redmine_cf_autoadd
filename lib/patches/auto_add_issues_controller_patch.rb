08e9596926ca11f33556a12ec313bb59cbc78163
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-02-03 11:23:06 +0100

    fix bugs

--- lib/patches/auto_add_issues_controller_patch.rb
+++ lib/patches/auto_add_issues_controller_patch.rb
@@ -15,31 +15,21 @@ module  Patches
          issues_cfs.each do |issue_cfs|
            detect_cf = @issue.visible_custom_field_values.detect{|cf| cf.custom_field == issue_cfs}
            if detect_cf
-             max = detect_cf.value
-             if detect_cf.value.nil? or detect_cf.value.empty?
-               if settings["cf_global_#{issue_cfs.name}"] == "true" # global increment
-                 cf = issue_cfs.custom_values.where("value is not null and value <> '' ")
-                 .where("value like '#{Time.now.strftime("%y")}_%'") # identify value for this year
-                 .order("id DESC").first
-               else
+             if settings["cf_global_#{issue_cfs.name}"] == "true" # global increment
+                 cf = issue_cfs.custom_values.where("value is not null and value <> '' ").order("id DESC").first
+             else
                  project = @issue.project
                  issues_id = project.issues.map(&:id)
                  cf = CustomValue.where("customized_type= 'issue' and customized_id in(?) and custom_field_id = ? and (value is not null and value <> '' ) ",
-                                        issues_id, issue_cfs.id)
-                 .where("value like '#{Time.now.strftime("%y")}_%'") # identify value for this year
-                 .order("id DESC").first
-               end
-
-               max = cf.value rescue nil
-               if settings["cf_auto_increment_#{issue_cfs.name}"] == "true"
-=begin
-                 arr = max.split('').reverse
-                 arr.pop(2)
-                 max = [Time.now.strftime("%y"), arr.reverse.join('').succ].join('')
-=end
-                 max = max.nil? ? settings['redmine_default_auto_add_value'] || [Time.now.strftime("%y"), '001'].join('_') : max.succ
-               end
+                                        issues_id, issue_cfs.id).order("id DESC").first
              end
+               max = cf.value rescue nil
+       
+               if settings["cf_auto_increment_#{issue_cfs.name}"] == "true"
+                 max = max.nil? ? issue_cfs.default_value : max.succ
+               else
+                 max = issue_cfs.default_value if max.nil?
+               end
              detect_cf.value = max
            end
 
